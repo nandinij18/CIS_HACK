0d1b044875ff0e1ca2180be15dc731ace3e2185c
nandinij18 <2420030262@klh.edu.in>
2026-04-09 10:55:34 +0530

    Update DB credentials, resolve TLS scraper errors, and shift to Port 8080

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const db = require('./db'); // Require our database connection
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
